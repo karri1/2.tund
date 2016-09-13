412e160d7df388e46447fcc4d9ee42eb7504d1f5
krnrk <krikkinen@gmail.com>
2016-09-13 13:10:04 +0300

    added for loop and if statement

---
+++ secondLesson.php
@@ -0,0 +1,24 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18){
+		echo "alaealine";
+	} else {
+		echo "täisealine";
+	}
+
+?>
+
+<br>
+
+<?php
+
+	//vanusele vastavalt trükime välja 'palju'
+	
+	for($i = 1; $i <= $age; $i++){            
+		echo $i . " palju ";
+	}
+	
+	echo "õnne!";
+?>
